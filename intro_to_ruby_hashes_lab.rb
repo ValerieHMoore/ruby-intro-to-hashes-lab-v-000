c285ce2ced46f339a2ac135cb46e5e5d1420b038
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 22:32:08 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {}
   monopoly = {
-    :railroads => {pieces: 4, names: {}, rent_in_dollars: {}}
+    :railroads => {pieces: 4, names: {}, rent_in_dollars: {one_piece_owned: 25}}
     }
 end
 
