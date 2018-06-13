a3b6b2d6b23ddf3bc9b74db36189557850f72650
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 22:27:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,21 +15,13 @@ end
 def monopoly_with_second_tier
 	monopoly = {}
   monopoly = {
-    :railroads => {
+    :railroads => {pieces: "4"},
       
     }}
   
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
-      ]
-   }, 
 
+
+  
 end
 
 def monopoly_with_third_tier
